64255aeee233103fcbec62d346584ba8041d4f0f
Kevin-Chen19 <108602152+Kevin-Chen19@users.noreply.github.com>
2026-01-19 17:40:37 +0800

    完善用户信息；完成团队界面的前端开发工作。

--- src/pages/team/team.vue
+++ src/pages/team/team.vue
@@ -1,3 +1,546 @@
+<!--文档回收站记得开发-->
 <template>
-  Team
+  <div class="bigBox">
+    <div class="bigTitle">Project Team</div>
+    <div class="Line_two">
+      <span class="smallText"> Welcome back,Kevin.Share team files here. </span>
+      <div class="searchBox">
+        <el-input
+          v-model="searchValue"
+          style="width: 100%"
+          placeholder="Search by name or position..."
+          class="search-input"
+          :prefix-icon="Search"
+          @change="toFind"
+        />
+      </div>
+    </div>
+    <div class="teamBox">
+      <div class="teamItem addItem" @click="inviteDialogVisible = true">
+        <div class="addBox">
+          <img src="@/assets/icons/加号.png" alt="加号" />
+        </div>
+        <div class="nameStyle">Add New Member</div>
+        <div class="positionStyle">Invite a new colleague to join</div>
+        <div class="positionStyle">the project team</div>
+      </div>
+      <div
+        class="teamItem"
+        v-for="item in showUsers"
+        :key="item.id"
+        @click="chooseUser(item)"
+      >
+        <div class="picBox">
+          <img :src="item.pic" alt="头像" />
+        </div>
+        <div class="nameStyle">{{ item.name }}</div>
+        <div class="positionStyle">{{ item.postion }}</div>
+        <div class="tags">
+          <div
+            class="tagItem"
+            v-for="(value, index) in item.tags.slice(0, 3)"
+            :key="index"
+          >
+            {{ value }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <el-drawer
+    v-model="showDrawer"
+    :modal="true"
+    :close-on-click-modal="true"
+    title="I am the title"
+    :direction="direction"
+    :before-close="handleClose"
+    :size="drawerWidth"
+  >
+    <template #header="{ close, titleId, titleClass }">
+      <h4 :id="titleId" :class="titleClass">Member Details</h4>
+    </template>
+    <div class="drawer_Box">
+      <div class="drawer_picBox">
+        <img :src="drawerUser.pic" alt="用户头像" />
+      </div>
+      <div class="drawer_Name">{{ drawerUser.name }}</div>
+      <div class="drawer_Email">{{ drawerUser.email }}</div>
+      <div
+        class="drawer_Status"
+        :class="{ drawer_Status_offline: drawerUser.status === 'offline' }"
+      >
+        {{ drawerUser.status }}
+      </div>
+      <div class="drawer_title">POSITION</div>
+      <div class="drawer_title drawer_Position">{{ drawerUser.postion }}</div>
+      <div class="drawer_title">ROLE</div>
+      <div class="drawer_title drawer_Position">
+        <el-select
+          v-model="drawerUser.role"
+          placeholder="Select"
+          style="width: 100%"
+          class="drawer_select"
+        >
+          <el-option
+            v-for="item in roleOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </div>
+      <div class="drawer_title">signature</div>
+      <div class="drawer_Position">
+        {{ drawerUser.signature }}
+      </div>
+      <div class="drawer_title">SKILLS</div>
+      <div class="tagsBox">
+        <div class="tagsItem" v-for="value in drawerUser.tags" :key="index">
+          {{ value }}
+        </div>
+      </div>
+    </div>
+    <template #footer>
+      <div class="chatBtn">Chat</div>
+      <div class="chatBtn saveBtn">Save</div>
+    </template>
+  </el-drawer>
+  <el-dialog
+    v-model="inviteDialogVisible"
+    title="Invite Members"
+    width="700"
+    align-center
+  >
+    <div class="line"></div>
+    <div class="inputName">EMAIL ADRESS</div>
+    <el-input
+      v-model="inviteData.emailContent"
+      placeholder="请输入邮箱地址"
+    ></el-input>
+    <div style="width: 100%; display: flex; justify-content: space-between">
+      <div>
+        <div class="inputName">ROLE ASSIGNMENT</div>
+        <el-select
+          v-model="inviteData.roleContent"
+          placeholder="Select Role"
+          style="width: 18rem"
+        >
+          <template #label="{ label, value }">
+            <span
+              >{{ label }}</span
+            >
+          </template>
+          <el-option
+            v-for="item in roles"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </div>
+      <div>
+        <div class="inputName">ADD TO PROJECT</div>
+        <el-select
+          v-model="inviteData.positionContent"
+          placeholder="Select Position"
+          style="width: 18rem"
+        >
+          <template #label="{ label, value }">
+            <span
+              >{{ label }}</span
+            >
+          </template>
+          <el-option
+            v-for="item in positions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </div>
+    </div>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="inviteDialogVisible = false" class="cancelBtn">
+          Cancel
+        </el-button>
+        <el-button type="primary" @click="submitInvite" class="confirmBtn">
+          Send Invites
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
+<script setup lang="ts">
+import { Search } from "@element-plus/icons-vue";
+import { useUserStore } from "@/stores/userStore";
+import { ref, computed, onMounted, onUnmounted, reactive } from "vue";
+const userStore = useUserStore();
+const searchValue = ref("");
+const showDrawer = ref(false);
+const inviteDialogVisible = ref(false);
+const windowWidth = ref(window.innerWidth);
+const drawerUser = reactive({});
+const showUsers = reactive([]);
+const inviteData = reactive({
+  emailContent: "",
+  roleContent:"",
+  positionContent:""
+})
+const roles = [
+  {
+    label:"viewer",
+    value:"viewer"
+  },
+  {
+    label:"contributor",
+    value:"contributor"
+  },
+   {
+    label:"manager",
+    value:"manager"
+  },
+   {
+    label:"admin",
+    value:"admin"
+  }
+]
+const positions = [
+  {
+    label:"Front-end Lead",
+    value:"Front-end Lead"
+  },
+  {
+    label:"Backend Lead",
+    value:"Backend Lead"
+  },
+   {
+    label:"UI Designer",
+    value:"UI Designer"
+  },
+   {
+    label:"DevOps",
+    value:"DevOps"
+  }
+]
+// 根据屏幕宽度计算抽屉宽度
+const drawerWidth = computed(() => {
+  if (windowWidth.value < 768) {
+    return "20rem"; // 移动端
+  } else if (windowWidth.value < 1500) {
+    return "20rem"; // 平板
+  } else {
+    return "20%"; // 桌面
+  }
+});
+const roleOptions = [
+  {
+    value: "admin",
+    label: "admin",
+  },
+  {
+    value: "manager",
+    label: "manager",
+  },
+  {
+    value: "contributor",
+    label: "contributor",
+  },
+  {
+    value: "viewer",
+    label: "viewer",
+  },
+];
+const chooseUser = (user) => {
+  drawerUser.name = user.name;
+  drawerUser.pic = user.pic;
+  drawerUser.postion = user.postion;
+  drawerUser.userId = user.userId;
+  drawerUser.tags = user.tags;
+  drawerUser.signature = user.signature;
+  drawerUser.email = user.email;
+  drawerUser.status = user.status;
+  drawerUser.role = user.role;
+  showDrawer.value = true;
+};
+// 监听窗口大小变化
+const handleResize = () => {
+  windowWidth.value = window.innerWidth;
+};
+const toFind = () => {
+  if (searchValue.value === "") {
+    showUsers.splice(0, showUsers.length, ...userStore.usersTable);
+  } else {
+    let newTable = [];
+    for (let item of userStore.usersTable) {
+      if (
+        item.name.toLowerCase().includes(searchValue.value.toLowerCase()) ||
+        item.postion.toLowerCase().includes(searchValue.value.toLowerCase())
+      ) {
+        newTable.push(item);
+      }
+    }
+    // 将newTable赋值给showTable
+    showUsers.splice(0, showUsers.length, ...newTable);
+  }
+};
+const submitInvite = () => {
+  inviteDialogVisible.value = false;
+}
+onMounted(() => {
+  window.addEventListener("resize", handleResize);
+  showUsers.push(...userStore.usersTable);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", handleResize);
+});
+</script>
+<style scoped lang="scss">
+.searchInput {
+  width: 20rem;
+}
+:deep(.search-input .el-input__wrapper) {
+  background-color: #fff;
+  border: none;
+  border-radius: 0;
+}
+:deep(.search-input .el-input__wrapper:focus-within) {
+  box-shadow: none;
+  border: 2px solid black;
+}
+.teamBox {
+  margin-top: 2rem;
+  box-sizing: border-box;
+  width: 100%;
+  max-height: 90%;
+  overflow: auto;
+  /* 隐藏标准滚动条 */
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* IE 10+ */
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.5rem 2.5%;
+  padding: 1rem;
+}
+.teamItem {
+  width: 23%;
+  min-width: 15rem;
+  height: 15rem;
+  background-color: #fff;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+}
+.teamItem:hover {
+  border: 2px solid #175e82;
+}
+.addItem {
+  background-color: transparent;
+  border: 2px dotted #75adc8;
+}
+.addItem:hover {
+  border: 2px dotted #035e8b;
+}
+.addBox {
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  border: 1px solid gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  background-color: #fff;
+  img {
+    width: 50%;
+  }
+}
+.picBox {
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  img {
+    width: 100%;
+  }
+}
+.nameStyle {
+  margin-top: 0.5rem;
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+.positionStyle {
+  font-size: 1rem;
+  color: gray;
+}
+.tags {
+  margin-top: 0.5rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+}
+.tagItem {
+  padding: 0.2rem 0.8rem;
+  background-color: aliceblue;
+  border-radius: 0.5rem;
+  color: gray;
+}
+.drawer_Box {
+  width: 100%;
+  height: 100%;
+  min-height: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.drawer_picBox {
+  margin-top: 2rem;
+  width: 6rem;
+  height: 6rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  img {
+    width: 100%;
+  }
+}
+.drawer_Name {
+  margin-top: 1rem;
+  font-size: 1.5rem;
+  font-weight: 500;
+}
+.drawer_Email {
+  font-size: 1rem;
+  color: gray;
+}
+.drawer_Status {
+  margin-top: 0.5rem;
+  font-size: 1rem;
+  padding: 0.2rem 1.5rem;
+  background-color: rgba(33, 230, 33, 0.79);
+  border-radius: 0.7rem;
+  color: rgb(16, 123, 16);
+}
+.drawer_Status_offline {
+  background-color: rgba(246, 104, 104, 0.79);
+  color: rgb(201, 4, 4);
+}
+.drawer_title {
+  width: 90%;
+  margin-top: 1rem;
+  font-size: 1rem;
+  color: gray;
+  font-weight: 500;
+}
+.drawer_Position {
+  margin-top: 0.5rem;
+  box-sizing: border-box;
+  background-color: #f7f8f9;
+  color: #717885;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 90%;
+  border: 1px solid rgb(206, 202, 202);
+}
+.tagsBox {
+  margin-top: 1rem;
+  width: 90%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+.tagsItem {
+  padding: 0.2rem 0.8rem;
+  background-color: #f7f8f9;
+  border-radius: 0.5rem;
+  color: #717885;
+}
+.chatBtn {
+  width: 90%;
+  box-sizing: border-box;
+  margin-left: 5%;
+  display: flex;
+  justify-content: center;
+  background-color: #175e82;
+  color: #fff;
+  font-size: 1rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  box-shadow: 5px 5px 10px rgb(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+.chatBtn:hover {
+  background-color: #04537b;
+}
+.saveBtn {
+  margin-top: 1rem;
+  background-color: rgba(56, 240, 56, 0.79);
+  color: rgb(16, 123, 16);
+}
+.saveBtn:hover {
+  background-color: rgba(22, 234, 22, 0.79);
+}
+:deep(.drawer_select .el-select__wrapper) {
+  background-color: transparent;
+  box-shadow: none;
+}
+:deep(.el-input__wrapper) {
+  box-sizing: border-box;
+  height: 2rem !important;
+  border-radius: 0 !important;
+  padding: 0.2rem 0.5rem !important;
+  background-color: #fff !important;
+  border: 1px solid #ccc;
+  box-shadow: none;
+}
+:deep(.el-input__wrapper:focus-within) {
+  border: 2px solid black;
+}
+:deep(.el-input__inner) {
+  font-weight: 500;
+  font-size: 1rem;
+}
+:deep(.el-select__wrapper:focus-within) {
+  border: 2px solid black;
+  box-shadow: none;
+}
+.cancelBtn {
+  height: 2.5rem;
+  border: none;
+  font-weight: 600;
+  font-size: larger;
+  margin-right: 2rem;
+  padding: 0 1.5rem;
+}
+.cancelBtn:hover {
+  background-color: #b5b7b8a9;
+  color: white;
+}
+.confirmBtn {
+  height: 2.5rem;
+  background-color: #175e82;
+  color: white;
+  font-weight: 600;
+  font-size: larger;
+  margin-right: 2rem;
+  border-radius: 0.5rem;
+  padding: 0 1.5rem;
+}
+.confirmBtn:hover{
+    background-color: #3bade7;
+}
+</style>
--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,40 +1,60 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 import user1 from "@/assets/pics/用户头像.jpg";
 import user2 from "@/assets/pics/用户2.jpg";
 import user3 from "@/assets/pics/用户3.jpg";
 import user4 from "@/assets/pics/用户4.jpg";
-export const useUserStore = defineStore('users', () => {
+export const useUserStore = defineStore("users", () => {
   const usersTable = [
-  {
-    name: "Kevin",
-    postion: "Frontend Lead",
-    percentage: 60,
-    pic: user1,
-    userId:1
-  },
-  {
-    name: "Sarah Jenkins",
-    postion: "Backend Lead",
-    percentage: 30,
-    pic: user2,
-    userId:2
-  },
-  {
-    name: "David Kim",
-    postion: "UI Designer",
-    percentage: 90,
-    pic: user3,
-    userId:3
-  },
-  {
-    name: "Mike Ross",
-    postion: "DevOps",
-    percentage: 50,
-    pic: user4,
-    userId:4
-  }
-]
+    {
+      name: "Kevin",
+      email: "Kevin@163.com",
+      status: "online",
+      postion: "Front-end Lead",
+      percentage: 60,
+      pic: user1,
+      userId: 1,
+      tags: ["Vue", "React", "node.js", "TypeScript", "Electron"],
+      signature: "前端技术专家，专注于前端技术，热爱开源，喜欢分享技术文章",
+      role: "admin",
+    },
+    {
+      name: "Sarah Jenkins",
+      email: "Sarah@163.com",
+      status: "online",
+      postion: "Backend Lead",
+      percentage: 30,
+      pic: user2,
+      userId: 2,
+      tags: ["Java", "SpringBoot", "MySQL"],
+      signature: "后端技术专家，专注于后端技术，热爱开源，喜欢分享技术文章",
+      role: "viewer",
+    },
+    {
+      name: "David Kim",
+      email: "David@163.com",
+      status: "online",
+      postion: "UI Designer",
+      percentage: 90,
+      pic: user3,
+      userId: 3,
+      tags: ["Figma"],
+      signature: "UI设计师，专注于UI设计，热爱开源，喜欢分享技术文章",
+      role: "manager",
+    },
+    {
+      name: "Mike Ross",
+      email: "Mike@163.com",
+      status: "offline",
+      postion: "DevOps",
+      percentage: 50,
+      pic: user4,
+      userId: 4,
+      tags: ["Vue", "React", "node.js", "TypeScript", "Electron"],
+      signature: "DevOps工程师，专注于DevOps技术，热爱开源，喜欢分享技术文章",
+      role: "contributor",
+    },
+  ];
 
-  return { usersTable }
-})
+  return { usersTable };
+});
