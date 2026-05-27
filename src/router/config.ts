2e369613f4f1ceefae6821c001e97ea6801403d1
Kevin-Chen19 <108602152+Kevin-Chen19@users.noreply.github.com>
2026-05-27 17:27:10 +0800

    清除无用文件与组件

--- src/router/config.ts
+++ src/router/config.ts
@@ -4,7 +4,6 @@ import tasks from "../pages/tasks/tasks.vue";
 import team from "../pages/team/team.vue";
 import calendar from "../pages/calendar/calendar.vue";
 import roles from "@/pages/roles/roles.vue";
-import settings from "@/pages/settings/settings.vue";
 import me from "@/pages/me/me.vue";
 const routesConfig = [
   {
@@ -34,10 +33,6 @@ const routesConfig = [
   {
     path: "/roles",
     component: roles,
-  },
-  {
-    path: "/settings",
-    component: settings,
   },
   {
     path: "/me",
